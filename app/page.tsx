40dc0ad1e65b4b62edf86da72b95693578171e00
itsteatv <rezashow60@gmail.com>
2024-05-10 14:48:42 +0330

    feat(client): Update Home component styling and content layout with responsive design

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,41 @@
+"use client";
+
+import { Button, Typography } from "@material-tailwind/react";
+import Image from "next/image";
+import girlIllustration from "@/public/girlIllustration.svg";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <section className="flex">
+      <div className="mt-16 flex-grow">
+        <p className="text-white ml-24 mt-20 font-Archivo font-extrabold text-sclamp 768<=:text-center 768<=:ml-auto 768<=:text-tclamp">
+          The Art of Blogging
+        </p>
+        <div className="text-left ml-24 768<=:ml-0 768<=:text-center mt-6">
+          <div className="">
+            <p className="text-gray-400 font-Archivo font-extralight text-fclamp 768<=:text-foclamp 768<=:mx-2">
+              Join us on this journey of exploration and discovery
+            </p>
+            <p className="text-gray-400 font-Archivo font-extralight text-fclamp 768<=:text-foclamp 768<=:mx-2">
+              as we delve into the fascinating world around us.
+            </p>
+            <p className="text-gray-400 font-Archivo font-extralight text-fclamp 768<=:text-foclamp 768<=:mx-2">
+              We look forward to sharing our knowledge and experiences with you!
+            </p>
+          </div>
+          <Button
+            ripple={true}
+            variant="outlined"
+            size="sm"
+            className="text-white border-white font-Archivo rounded-full w-28 mt-6 font-extralight"
+          >
+            Explore
+          </Button>
+        </div>
+      </div>
+      <div className="mt-16 1180>=:mt-28 lg:mr-28 lg:w-[30rem] 930>=:w-[25rem] 930>=:mt-[5.5rem] 768<=:hidden">
+        <Image src={girlIllustration} alt="blog image" />{" "}
+      </div>
+    </section>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,12 +10,21 @@ const config: Config = withMT({
   ],
   theme: {
     extend: {
+      fontSize: {
+        fclamp: "clamp(0.5rem, 0.38636363636363635rem + 1.1363636363636365vw, 1.75rem)",
+        sclamp: "clamp(1.25rem, -1.25rem + 5.208333333333333vw, 5rem)",
+        tclamp: "clamp(0.5rem, -0.07692307692307687rem + 6.41025641025641vw, 3rem)",
+        foclamp: "clamp(0.5rem, 0.38461538461538464rem + 1.282051282051282vw, 1rem)",
+      },
       fontFamily: {
         "FiraSans": ["Fira Sans", "sans-serif"],
         "Archivo": ["Archivo", "sans-serif"],
       },
       screens: {
         "1140>=": { "min": "9.375em", "max": "71.25em" },
+        "1180>=": { "min": "60em", "max": "73.75em" },
+        "930>=": { "min": "48em", "max": "58.125em" },
+        "768<=": { "max": "48em" },
       }
     },
   },
