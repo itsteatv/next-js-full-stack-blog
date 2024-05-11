165dae6c1e48185deb6234264463905e57137e6b
itsteatv <rezashow60@gmail.com>
2024-05-11 13:51:47 +0330

    feat(about): Update About page UI with responsive design and content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,47 @@
+"use client";
+
+import { Button } from "@material-tailwind/react";
+import Image from "next/image";
+import boyIllustration from "@/public/boyIllustration.svg";
+import Link from "next/link";
+import Footer from "../components/footer/Footer";
+
 const About = () => {
-  return <div>page</div>;
+  return (
+    <section className="flex">
+      <div className="mt-16 1180>=:mt-28 lg:mr-28 lg:w-[30rem] 930>=:w-[25rem] 930>=:mt-[5.5rem] 768<=:hidden">
+        <Image src={boyIllustration} alt="blog image" />{" "}
+      </div>
+      <div className="mt-16 flex-grow">
+        <p className="text-white mr-24 mt-20 font-Archivo font-extrabold text-sclamp 768<=:text-center 768<=:mr-auto 768<=:text-tclamp">
+          Next.js 14 Full Stack Blog
+        </p>
+        <div className="text-left mr-24 768<=:mr-0 768<=:text-center mt-6">
+          <div className="">
+            <p className="text-gray-400 font-Archivo font-extralight text-fclamp 768<=:text-foclamp 768<=:mx-2">
+              Welcome to our blog! We're passionate about exploring the world{" "}
+            </p>
+            <p className="text-gray-400 font-Archivo font-extralight text-fclamp 768<=:text-foclamp 768<=:mx-2">
+              and sharing our discoveries with you.
+            </p>
+            <p className="text-gray-400 font-Archivo font-extralight text-fclamp 768<=:text-foclamp 768<=:mx-2">
+              Join us as we journey through fascinating topics.
+            </p>
+          </div>
+          <Button
+            ripple={true}
+            variant="outlined"
+            size="sm"
+            className="text-white border-white font-Archivo rounded-full w-28 mt-6 font-extralight"
+          >
+            <Link href="/contact">Contact Us</Link>
+          </Button>
+        </div>
+      </div>
+
+      <Footer />
+    </section>
+  );
 };
 
 export default About;
---
+++ app/components/footer/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="fixed bottom-0 mb-7 w-full">
+      <div className="flex items-center justify-between 450>=:flex-col 450>=:gap-y-1">
+        <p className="text-gray-400 font-Archivo ml-24 450>=:ml-0">itsteatv</p>
+        <p className="text-xs text-gray-400 font-Archivo mr-24 450>=:mr-0">
+          © 2024 itsteatv. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
