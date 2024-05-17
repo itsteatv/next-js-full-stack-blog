75cdcc70d0546b5bcab182d50e3be4da2feb6b9b
itsteatv <rezashow60@gmail.com>
2024-05-17 09:32:49 +0330

    feat(404): Add custom 404 Not Found page

---
+++ app/not-found.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import Button from "./components/button/Button";
+
+const NotFound = () => {
+  return (
+    <div className="mt-36 px-4 dark:bg-gray-900">
+      <div className="text-center">
+        <h1 className="text-9xl font-black text-white">404</h1>
+
+        <p className="text-2xl font-bold tracking-tight text-gray-300 sm:text-4xl">
+          Uh-oh!
+        </p>
+
+        <p className="mt-4 text-gray-300">We can't find that page.</p>
+
+        <Link href="/">
+          <Button
+            content="Go Back Home"
+            className="text-white border-white font-Archivo rounded-full  mt-6 font-extralight"
+            ripple={true}
+            variant="gradient"
+          />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
