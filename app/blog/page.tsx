102550d15b9fcbecac572196bff1d394476313d2
itsteatv <rezashow60@gmail.com>
2024-05-24 11:58:19 +0330

    feat(blog): Add BlogCard component and integrate into Blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,16 @@
+import BlogCard from "../components/blogCard/BlogCard";
+
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <main className="grid gap-4 px-16 py-16 md:grid-cols-2 lg:grid-cols-3">
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+    </main>
+  );
 };
 
 export default Blog;
---
+++ app/components/blogCard/BlogCard.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+} from "@material-tailwind/react";
+
+import Image from "next/image";
+import Button from "../button/Button";
+
+const BlogCard = () => {
+  return (
+    <Card className="max-w-[24rem] overflow-hidden">
+      <CardHeader
+        floated={false}
+        shadow={false}
+        color="transparent"
+        className="m-0 rounded-none"
+      >
+        <Image
+          src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
+          alt="ui/ux review check"
+          width={450}
+          height={450}
+        />
+      </CardHeader>
+      <CardBody>
+        <Typography variant="h5" color="blue-gray" className="font-Archivo">
+          UI/UX Review Check
+        </Typography>
+        <Typography
+          variant="paragraph"
+          color="gray"
+          className="mt-3 font-light font-FiraSans"
+        >
+          Because it&apos;s about motivating the doers. Because I&apos;m here to
+          follow my dreams and inspire others.
+        </Typography>
+      </CardBody>
+      <CardFooter className="flex items-center justify-between">
+        <div className="flex items-center -space-x-3">
+          <Button
+            content="Read more"
+            type="button"
+            ripple={true}
+            size="sm"
+            variant="outlined"
+            className="hover:bg-black hover:text-white duration-300 font-Archivo"
+          />
+        </div>
+        <Typography variant="small" className="font-light font-FiraSans">
+          January 10
+        </Typography>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default BlogCard;
