2acc2e0a0b360b075da69e0877f368397f54bfed
itsteatv <rezashow60@gmail.com>
2024-07-24 00:02:26 +0330

    fix: Correct typo in formatDate utility function

--- lib/formateDate.ts
+++ lib/formateDate.ts
@@ -1,4 +1,4 @@
-export const formateData = function (dataString: string) {
+export const formateDate = function (dataString: string) {
     const data = new Date(dataString)
     return data.toLocaleDateString("en-US", {
         year: "numeric",
