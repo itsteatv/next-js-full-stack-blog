a3919b71e207b96a4e51f4b5e97c39d1a94efd3d
itsteatv <rezashow60@gmail.com>
2024-07-27 22:32:08 +0330

    fix: Update Next.js image configuration to use remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,16 @@
 
 const imageDomains = process.env.NEXT_PUBLIC_IMAGE_DOMAINS?.split(',') || [];
 
+const remotePatterns = imageDomains.map(domain => ({
+    protocol: 'https',
+    hostname: domain,
+    pathname: '/**'
+}));
+
 const nextConfig = {
     images: {
         dangerouslyAllowSVG: true,
-        domains: imageDomains,
+        remotePatterns,
     },
 };
 
