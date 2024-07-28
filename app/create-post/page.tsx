7f3beb22228b8cb6abcb29259c9cb6bcdeff2724
itsteatv <rezashow60@gmail.com>
2024-07-28 23:26:52 +0330

    feat: Enhance Create Post page with form elements

--- app/create-post/page.tsx
+++ app/create-post/page.tsx
@@ -1,5 +1,36 @@
+import Button from "@/components/Button";
+import Input from "@/components/Input";
+
 const page = () => {
-  return <div>create-post</div>;
+  return (
+    <section className="mt-20">
+      <div className="flex flex-col items-center justify-center">
+        <form className="mb-4 rounded-3xl w-full max-w-60">
+          <div className="mb-4">
+            <label
+              className="block text-white text-sm font-bold mb-2"
+              htmlFor="name"
+            >
+              Title
+            </label>
+            <Input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+              placeholder="Your title"
+              name="title"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <Button
+              className="inline-block w-full cursor-pointer rounded-xl disabled:bg-gray-500 disabled:cursor-not-allowed bg-black px-8 py-4 mt-4 text-center duration-300 font-semibold text-white no-underline [box-shadow:rgb(19,_83,_254)_6px_6px]"
+              type="submit"
+              content="Create"
+            />
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default page;
