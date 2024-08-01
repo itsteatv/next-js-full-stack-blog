4ba265feb10d077e8664af8004bcd4b3adaec52e
itsteatv <rezashow60@gmail.com>
2024-08-02 00:17:53 +0330

    feat: Enhance navigation with authentication-based links and buttons

--- components/NavList.tsx
+++ components/NavList.tsx
@@ -5,6 +5,7 @@ import {
 } from "@kinde-oss/kinde-auth-nextjs";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import Button from "./Button";
 
 const NavList = () => {
   const pathname = usePathname();
@@ -23,46 +24,88 @@ const NavList = () => {
 
   return (
     <ul className="my-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-      <p
-        className={`p-1 duration-150 hover:text-gray-500 text-[#d6d6d6] font-Archivo ${isActive(
-          "/blog"
-        )}`}
-      >
-        <a href="/blog" className="flex items-center transition-colors">
-          Blog
-        </a>
-      </p>
-      <p
-        className={`p-1 duration-150 hover:text-gray-500 text-[#d6d6d6] font-Archivo ${isActive(
-          "/contact"
-        )}`}
-      >
-        <Link href="/contact" className="flex items-center transition-colors">
-          Contact
-        </Link>
-      </p>
-      <p
-        className={`p-1 duration-150 hover:text-gray-500 text-[#d6d6d6] font-Archivo ${isActive(
-          "/about"
-        )}`}
-      >
-        <Link href="/about" className="flex items-center transition-colors">
-          About
-        </Link>
-      </p>
-      <p
-        className={`p-1 duration-150 hover:text-gray-500 text-[#d6d6d6] font-Archivo ${isActive(
-          "/about"
-        )}`}
-      >
-        <Link
-          href="/create-post"
-          className="flex items-center transition-colors"
+      {[
+        { href: "/blog", text: "Blog" },
+        { href: "/contact", text: "Contact" },
+        { href: "/about", text: "About" },
+      ].map(({ href, text }) => (
+        <li
+          key={href}
+          className={`p-1 duration-150 hover:text-gray-500 text-[#d6d6d6] font-Archivo ${isActive(
+            href
+          )}`}
         >
-          Create Post
-        </Link>
-      </p>
-      {isAuthenticated && <LogoutLink>Logout</LogoutLink>}
+          <Link href={href} className="flex items-center transition-colors">
+            {text}
+          </Link>
+        </li>
+      ))}
+
+      {isAuthenticated ? (
+        <>
+          <li
+            className={`p-1 duration-150 hover:text-gray-500 text-[#d6d6d6] font-Archivo ${isActive(
+              "/create-post"
+            )}`}
+          >
+            <Link
+              href="/create-post"
+              className="flex items-center transition-colors"
+            >
+              Create Post
+            </Link>
+          </li>
+          <li>
+            <LogoutLink>
+              <Button
+                content="Logout"
+                type="button"
+                color="red"
+                ripple
+                size="sm"
+                variant="gradient"
+                className="text-white font-bold"
+              />
+            </LogoutLink>
+          </li>
+        </>
+      ) : (
+        <>
+          <li
+            className={`p-1 duration-150 hover:text-gray-500 text-[#d6d6d6] font-Archivo ${isActive(
+              "/login"
+            )}`}
+          >
+            <Link href="/login" className="flex items-center transition-colors">
+              <Button
+                content="Login"
+                type="button"
+                size="sm"
+                variant="filled"
+                className="font-bold"
+              />
+            </Link>
+          </li>
+          <li
+            className={`p-1 duration-150 hover:text-gray-500 text-[#d6d6d6] font-Archivo ${isActive(
+              "/register"
+            )}`}
+          >
+            <Link
+              href="/register"
+              className="flex items-center transition-colors"
+            >
+              <Button
+                content="Register"
+                type="button"
+                size="sm"
+                variant="filled"
+                className="font-bold"
+              />
+            </Link>
+          </li>
+        </>
+      )}
     </ul>
   );
 };
