8e5d7b8c37446d76c0ed58e36112507d25b0db8a
itsteatv <rezashow60@gmail.com>
2024-08-07 21:20:52 +0330

    feat: Add authentication check and redirect in createPost function

--- actions/createPost.ts
+++ actions/createPost.ts
@@ -1,9 +1,18 @@
 "use server"
 
 import prisma from "@/lib/db"
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import { revalidatePath } from "next/cache"
+import { redirect } from "next/navigation"
 
 export default async function createPost(formData: FormData) {
+
+    const { isAuthenticated } = getKindeServerSession()
+
+    if (!(await isAuthenticated())) {
+        redirect("/api/auth/login")
+    }
+
     const title = formData.get("title") as string
     const body = formData.get("body") as string
 
