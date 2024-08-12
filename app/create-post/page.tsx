bddb766baf5cfa31a10e6317d20c425f92604265
itsteatv <rezashow60@gmail.com>
2024-08-12 21:25:20 +0330

    feat: Add CreatePostForm component and integrate into create-post page

--- app/create-post/page.tsx
+++ app/create-post/page.tsx
@@ -1,7 +1,4 @@
-import createPost from "@/actions/createPost";
-import Button from "@/components/Button";
-import Input from "@/components/Input";
-import TextArea from "@/components/TextArea";
+import CreatePostForm from "@/components/CreatePostForm";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 
@@ -15,43 +12,7 @@ const page = async () => {
   return (
     <section className="mt-20">
       <div className="flex flex-col items-center justify-center">
-        <form action={createPost} className="mb-4 rounded-3xl w-full max-w-60">
-          <div className="mb-4">
-            <label
-              className="block text-white text-sm font-bold mb-2"
-              htmlFor="title"
-            >
-              Title
-            </label>
-            <Input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
-              type="text"
-              placeholder="Your title"
-              name="title"
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-white text-sm font-bold mb-2"
-              htmlFor="body"
-            >
-              Body
-            </label>
-            <TextArea
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
-              rows={4}
-              name="body"
-              placeholder="Your body"
-            />
-          </div>
-          <div className="flex items-center justify-between">
-            <Button
-              className="inline-block w-full cursor-pointer rounded-xl disabled:bg-gray-500 disabled:cursor-not-allowed bg-black px-8 py-4 mt-4 text-center duration-300 font-semibold text-white no-underline [box-shadow:rgb(19,_83,_254)_6px_6px]"
-              type="submit"
-              content="Create"
-            />
-          </div>
-        </form>
+        <CreatePostForm />
       </div>
     </section>
   );
---
+++ components/CreatePostForm.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import createPost from "@/actions/createPost";
+import Button from "@/components/Button";
+import Input from "@/components/Input";
+import TextArea from "@/components/TextArea";
+
+const CreatePostForm = () => {
+  return (
+    <form action={createPost} className="mb-4 rounded-3xl w-full max-w-60">
+      <div className="mb-4">
+        <label
+          className="block text-white text-sm font-bold mb-2"
+          htmlFor="title"
+        >
+          Title
+        </label>
+        <Input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
+          type="text"
+          placeholder="Your title"
+          name="title"
+        />
+      </div>
+      <div className="mb-4">
+        <label
+          className="block text-white text-sm font-bold mb-2"
+          htmlFor="body"
+        >
+          Body
+        </label>
+        <TextArea
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
+          rows={4}
+          name="body"
+          placeholder="Your body"
+        />
+      </div>
+      <div className="flex items-center justify-between">
+        <Button
+          className="inline-block w-full cursor-pointer rounded-xl disabled:bg-gray-500 disabled:cursor-not-allowed bg-black px-8 py-4 mt-4 text-center duration-300 font-semibold text-white no-underline [box-shadow:rgb(19,_83,_254)_6px_6px]"
+          type="submit"
+          content="Create"
+        />
+      </div>
+    </form>
+  );
+};
+
+export default CreatePostForm;
