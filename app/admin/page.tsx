378eb6e04e8d2b1536405b06091e5d733b7adf88
itsteatv <rezashow60@gmail.com>
2024-08-21 04:12:33 +0330

    feat: Add authentication and authorization to Admin component

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,28 @@
-const Admin = () => {
-  return <div>Admin</div>;
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+
+const Admin = async () => {
+  const { isAuthenticated, getPermission } = getKindeServerSession();
+
+  const isLoggedIn = await isAuthenticated();
+
+  if (!isLoggedIn) {
+    redirect("/api/auth/login");
+  }
+
+  const requiredPermission = await getPermission("all::permissions");
+
+  if (!requiredPermission?.isGranted) {
+    redirect("/");
+  }
+
+  return (
+    <>
+      <h1 className="text-white text-4xl flex items-center justify-center">
+        Admin Dashboard
+      </h1>
+    </>
+  );
 };
 
 export default Admin;
