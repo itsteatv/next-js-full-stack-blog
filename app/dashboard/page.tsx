445b39d9b9c8814f6044b03464116cec1eb6ea3c
itsteatv <rezashow60@gmail.com>
2024-08-24 02:47:25 +0330

    feat(dashboard): Add protected Dashboard component to display user information

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,23 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+
+const Dashboard = async () => {
+  const { getUser, isAuthenticated } = getKindeServerSession();
+  const user = await getUser();
+
+  const isLoggedIn = await isAuthenticated();
+
+  if (!isLoggedIn) {
+    redirect("/api/auth/login");
+  }
+  return (
+    <div className="text-white flex items-center flex-col mt-16">
+      <p>Given Name: {user?.given_name}</p>
+      <p>Username: {user?.username}</p>
+      <p>Email: {user?.email}</p>
+      <p>ID: {user?.id}</p>
+    </div>
+  );
+};
+
+export default Dashboard;
