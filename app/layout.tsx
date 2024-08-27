722341b35752380aee956ac981d5d3f8f232a068
itsteatv <rezashow60@gmail.com>
2024-08-27 21:19:52 +0330

    style(root-layout): Update global HTML styling with background gradient

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="bg-[#000000]">
+    <html
+      lang="en"
+      className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"
+    >
       <Providers>
         <body className={inter.className}>
           <NextTopLoader showSpinner={false} color="#fff" />
