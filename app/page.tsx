fa4e902a381b3d628d04a9bed4bd969b65d26cfd
itsteatv <104348533+itsteatv@users.noreply.github.com>
2024-12-27 22:49:05 +0330

    feat(homepage): Integrate Footer component into the Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import girlIllustration from "@/public/girlIllustration.svg";
 import Button from "@/components/Button";
 import Link from "next/link";
+import Footer from "@/components/Footer";
 
 export const metadata = {
   title: "Home",
@@ -9,35 +10,39 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <section className="flex">
-      <div className="mt-16 flex-grow">
-        <p className="dark:text-white ml-24 mt-20 font-Archivo font-extrabold text-sclamp 768<=:text-center 768<=:ml-auto 768<=:text-tclamp">
-          The Art of Blogging
-        </p>
-        <div className="text-left ml-24 768<=:ml-0 768<=:text-center mt-6">
-          <div className="">
-            <p className="dark:text-gray-400 font-Archivo font-extralight text-fclamp 768<=:text-foclamp 768<=:mx-2">
-              Join us on this journey of exploration and discovery
-            </p>
-            <p className="dark:text-gray-400 font-Archivo font-extralight text-fclamp 768<=:text-foclamp 768<=:mx-2">
-              as we delve into the fascinating world around us.
-            </p>
-            <p className="dark:text-gray-400 font-Archivo font-extralight text-fclamp 768<=:text-foclamp 768<=:mx-2">
-              We look forward to sharing our knowledge and experiences with you!
-            </p>
+    <>
+      <section className="flex items-center justify-center h-screen">
+        <div className="mt-16 flex-grow">
+          <p className="dark:text-white ml-24 mt-20 font-Archivo font-extrabold text-sclamp 768<=:text-center 768<=:ml-auto 768<=:text-tclamp">
+            The Art of Blogging
+          </p>
+          <div className="text-left ml-24 768<=:ml-0 768<=:text-center mt-6">
+            <div className="">
+              <p className="dark:text-gray-400 font-Archivo font-extralight text-fclamp 768<=:text-foclamp 768<=:mx-2">
+                Join us on this journey of exploration and discovery
+              </p>
+              <p className="dark:text-gray-400 font-Archivo font-extralight text-fclamp 768<=:text-foclamp 768<=:mx-2">
+                as we delve into the fascinating world around us.
+              </p>
+              <p className="dark:text-gray-400 font-Archivo font-extralight text-fclamp 768<=:text-foclamp 768<=:mx-2">
+                We look forward to sharing our knowledge and experiences with
+                you!
+              </p>
+            </div>
+            <Link href="/blog">
+              <Button
+                type="button"
+                className="rounded-md bg-white/10 px-3.5 duration-300 py-2.5 mt-6 text-sm font-semibold dark:text-white shadow-sm dark:hover:bg-white/20 hover:bg-gray-100 ring-1 ring-inset ring-gray-300 dark:ring-0"
+                label="Explore"
+              />
+            </Link>
           </div>
-          <Link href="/blog">
-            <Button
-              type="button"
-              className="rounded-md bg-white/10 px-3.5 duration-300 py-2.5 mt-6 text-sm font-semibold dark:text-white shadow-sm dark:hover:bg-white/20 hover:bg-gray-100 ring-1 ring-inset ring-gray-300 dark:ring-0"
-              label="Explore"
-            />
-          </Link>
         </div>
-      </div>
-      <div className="mt-16 1180>=:mt-28 lg:mr-28 lg:w-[30rem] 930>=:w-[25rem] 930>=:mt-[5.5rem] 768<=:hidden">
-        <Image src={girlIllustration} alt="blog image" />{" "}
-      </div>
-    </section>
+        <div className="mt-16 1180>=:mt-28 lg:mr-28 lg:w-[30rem] 930>=:w-[25rem] 930>=:mt-[5.5rem] 768<=:hidden">
+          <Image src={girlIllustration} alt="blog image" />{" "}
+        </div>
+      </section>
+      <Footer />
+    </>
   );
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,44 @@
+import Link from "next/link";
 import React from "react";
+
+const navigation = [
+  {
+    name: "GitHub",
+    href: "https://github.com/itsteatv",
+    icon: (props: any) => (
+      <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+        <path
+          fillRule="evenodd"
+          d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
+  },
+];
 
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 mb-7 w-full">
-      <div className="flex items-center justify-between 450>=:flex-col 450>=:gap-y-1">
-        <p className="text-gray-400 font-Archivo ml-24 450>=:ml-0">itsteatv</p>
-        <p className="text-xs text-gray-400 font-Archivo mr-24 450>=:mr-0">
-          © 2024 itsteatv. All rights reserved.
-        </p>
+    <footer className="dark:bg-[#121212]">
+      <div className="mx-auto max-w-7xl px-6 pt-32 pb-6 md:py-12 md:flex md:items-center md:justify-between lg:px-8">
+        <div className="flex justify-center space-x-6 md:order-2">
+          {navigation.map((item) => (
+            <Link
+              key={item.name}
+              href={item.href}
+              target="_blank"
+              className="dark:text-white dark:hover:text-gray-400 hover:text-gray-700 duration-300"
+            >
+              <span className="sr-only">{item.name}</span>
+              <item.icon aria-hidden="true" className="h-6 w-6" />
+            </Link>
+          ))}
+        </div>
+        <div className="mt-8 md:order-1 md:mt-0">
+          <p className="text-center text-xs leading-5 dark:text-gray-500">
+            &copy; 2020 Your Company, Inc. All rights reserved.
+          </p>
+        </div>
       </div>
     </footer>
   );
