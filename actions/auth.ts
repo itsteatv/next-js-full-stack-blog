f154b6062fabc6edbc62777f5410cbde49ef2415
itsteatv <104348533+itsteatv@users.noreply.github.com>
2025-02-20 22:22:39 +0330

    feat(auth): Integrate server actions for authentication

---
+++ actions/auth.ts
@@ -0,0 +1,74 @@
+"use server";
+
+import { createClient } from "@/utils/supabase/server";
+import { revalidatePath } from "next/cache";
+
+export async function register(formData: FormData) {
+  console.log(formData);
+
+  const supabase = await createClient();
+
+  const credentials = {
+    username: formData.get("username") as string,
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
+
+  console.log(credentials);
+
+  const { error, data } = await supabase.auth.signUp({
+    email: credentials.email,
+    password: credentials.password,
+    options: {
+      data: {
+        username: credentials.username,
+      },
+    },
+  });
+
+  console.log(error);
+
+  if (error) {
+    return {
+      status: error.message,
+      user: null,
+    };
+  } else if (data.user?.identities?.length === 0) {
+    return {
+      status: "User with this email already exists",
+      user: null,
+    };
+  }
+
+  revalidatePath("/en", "layout");
+
+  console.log(data.user);
+
+  return { status: "success", user: data.user };
+}
+
+export async function signIn(formData: FormData) {
+  const supabase = await createClient();
+
+  const credentials = {
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
+
+  const { error, data } = await supabase.auth.signInWithPassword(credentials);
+
+  if (error) {
+    return {
+      status: error.message,
+      user: null,
+    };
+  }
+
+  console.log(error);
+
+  revalidatePath("/en", "layout");
+
+  console.log(data.user);
+
+  return { status: "success", user: data.user };
+}
--- components/Auth/SignInForm.tsx
+++ components/Auth/SignInForm.tsx
@@ -1,23 +1,50 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
+import { signIn } from "@/actions/auth";
 
 const SignInForm = () => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
   const pathname = usePathname();
   const locale = pathname.split("/")[1] || "en";
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setLoading(true);
+    setError(null);
+
+    const formData = new FormData(event.currentTarget);
+    console.log(formData);
+    const result = await signIn(formData);
+
+    console.log(result);
+
+    if (result.status === "success") {
+      router.push(`/${locale}/blog`);
+    } else {
+      setError(result.status);
+    }
+
+    setLoading(false);
+  };
 
   return (
     <div className="sm:max-w-smd mx-auto flex items-center justify-center min-h-screen ">
       <div className="p-8 rounded-lg shadow-lg w-full max-w-sm">
         <h2 className="text-2xl font-bold text-center mb-6">Sign In</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="w-full mb-4">
             <div className="input-group w-full">
               <span className="input-group-text">
                 <span className="icon-[solar--mention-square-bold-duotone] text-base-content/80 size-5"></span>
               </span>
               <input
+                id="email"
+                name="email"
                 type="email"
                 className="input max-w-sm"
                 placeholder="Enter your email"
@@ -30,6 +57,8 @@ const SignInForm = () => {
                 <span className="icon-[solar--lock-password-bold-duotone] text-base-content/80 size-5"></span>
               </span>
               <input
+                name="password"
+                id="password"
                 type="password"
                 className="input max-w-sm"
                 placeholder="Enter your password"
