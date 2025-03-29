bc433c9f5ebb2f78a5fc029b23cf0db506e5929a
itsteatv <104348533+itsteatv@users.noreply.github.com>
2025-03-30 00:47:12 +0430

    feat(download-user-data): Improve data fetching and validation

--- actions/download.ts
+++ actions/download.ts
@@ -19,25 +19,22 @@ export async function downloadUserData() {
   const { data, error } = await supabase
     .from("user_profiles")
     .select("id, email, username, first_name, last_name, created_at")
-    .eq("id", user.id);
-
-  if (data?.length) {
-    data[0].email = maskEmail(data[0].email);
-  }
+    .eq("id", user.id)
+    .single();
 
   if (error) {
     throw new Error("An error occurred. Please try again later.");
   }
 
-  if (!data || data.length === 0) {
+  if (!data) {
     throw new Error("User data not found.");
   }
 
-  try {
-    const validatedUser = download.parse(data[0]);
-    validatedUser.email = maskEmail(validatedUser.email);
-    return JSON.stringify(validatedUser, null, 2);
-  } catch (validationError) {
-    throw new Error("Invalid user data format.");
-  }
+
+  const validatedUser = download.parse(data);
+  console.log("Validated User:", validatedUser);
+
+  validatedUser.email = maskEmail(validatedUser.email);
+
+  return JSON.stringify(validatedUser, null, 2);
 }
